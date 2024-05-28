903aac6a4c18e919944bd2714be5e94a976f90ac
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2024-05-28 14:52:33 -0300

    usa for Each na construção das tabelas

--- TCC/Views/Components/TableView.swift
+++ TCC/Views/Components/TableView.swift
@@ -3,9 +3,6 @@ import SwiftUI
 struct TableView: View {
     let titles = ["Capacidade do\nReservatório",
                   "Potencial de\natendimento\nda demanda",
-                  "Meses do ano\ncompletamente\natendidos",
-                  "Meses do ano\nparcialmente\natendidos",
-                  "Meses do ano\nnão atendidos",
                   "Investimento \nInicial (R$)",
                   "Tempo de Retorno\ndo Investimento\n(anos)"]
     @Binding var dadosSimulacao: [SumarioDados]
@@ -28,9 +25,6 @@ struct TableView: View {
                         GridRow {
                             Text("\(String(format: "%.0f", dado.capacidadeReservatorio))")
                             Text("\(String(format: "%.2f", dado.potencialMedioDeAtendimentoDaDemanda * 100))%")
-                            Text("\(String(format: "%.2f", dado.mesesAtendidosCompletamente))")
-                            Text("\(String(format: "%.2f", dado.mesesAtendidosParcialmente))")
-                            Text("\(String(format: "%.2f", dado.mesesNaoAtendidos))")
                             Text("A")
                             Text("B")
                             NavigationLink {
