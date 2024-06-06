50e6a3fc760515d3aae4f8be4e1dc0c5960ab1ec
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2024-06-06 08:30:24 -0300

    primeira versao postada na app store

--- TCC/Views/TemasDidaticosView.swift
+++ TCC/Views/TemasDidaticosView.swift
@@ -9,7 +9,7 @@ struct TemasDidaticosView: View {
             VStack {
                 List{
                     Section {
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "gearshape",
                                             texto: "Funcionamento do Processo",
                                             cor: .gray)
@@ -18,27 +18,27 @@ struct TemasDidaticosView: View {
                                             texto: "Usos da Água Tratada",
                                             cor: .orange)
                         
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "drop",
                                             texto: "Problema da Água no Mundo",
                                             cor: .blue)
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "cloud.drizzle",
                                             texto: "Índice pluviométrico",
                                             cor: .indigo)
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "shippingbox",
                                             texto: "Materiais da Operação",
                                             cor: .brown)
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "arrow.circlepath",
                                             texto: "Troca do Clorador",
                                             cor: .black)
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "rectangle",
                                             texto: "Como calcular a área de captação",
                                             cor: .red)
-                        BotaoTemasDidaticos(destino: Text("AAA"),
+                        BotaoTemasDidaticos(destino: UsosDaAguaTratada(),
                                             imagem: "exclamationmark.triangle",
                                             texto: "Limitações do projeto",
                                             cor: Color.yellow)
